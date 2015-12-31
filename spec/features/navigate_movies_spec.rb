78eaeb2bfb670d6e3bcbc22a42aade89034e2697
Arman Jon Villalobos <me@armanjon.com>
2015-12-31 21:56:56 +0800

    Add test for navigating between listing page and show page

---
+++ spec/features/navigate_movies_spec.rb
@@ -0,0 +1,19 @@
+describe "Navigating movies" do
+  it "allows navigation from the details page to the listing page" do
+    movie = Movie.create(movie_attributes)
+
+    visit movie_path(movie)
+    click_link "All Movies"
+
+    expect(current_path).to eq(movies_path)
+  end
+
+  it "allows navigation from the listing page to the details page" do 
+    movie = Movie.create(movie_attributes)
+
+    visit movies_path
+    click_link movie.title
+
+    expect(current_path).to eq(movie_path(movie))
+  end
+ end
