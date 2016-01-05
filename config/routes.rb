49b67cad263f91e67feb4ac318cc21faf8c21456
Arman Jon Villalobos <me@armanjon.com>
2016-01-05 22:39:55 +0800

    Add edit form and edit link

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "movies#index"
   get "movies" => "movies#index"
   get "movie/:id" => "movies#show", as: 'movie'
+  get 'movie/:id/edit' => "movies#edit", as: 'edit_movie'
 end
