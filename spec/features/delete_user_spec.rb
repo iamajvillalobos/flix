01f5f902c2f1a7fbd3e61c8ef5516d26219e6591
Arman Jon Villalobos <me@armanjon.com>
2016-01-17 22:11:49 +0800

    Update test

--- spec/features/delete_user_spec.rb
+++ spec/features/delete_user_spec.rb
@@ -2,16 +2,12 @@ describe "Deleting a user" do
   it "destroys the user and shows the user listing without the deleted user" do
     user = User.create!(user_attributes)
 
+    sign_in(user)
     visit user_path(user)
 
     click_link 'Delete Account'
 
-    expect(current_path).to eq(users_path)
-    expect(page).to have_text('Account successfully deleted!')
-
-    visit users_path
-
-    expect(page).not_to have_text(user.name)
+    expect(current_path).to eq(new_session_path)
   end
 
   it "automatically signs out the user" do
--- spec/features/edit_user_spec.rb
+++ spec/features/edit_user_spec.rb
@@ -3,6 +3,7 @@ describe "Editing a user" do
   it "updates the user and shows the user's updated details" do
     user = User.create!(user_attributes)
 
+    sign_in(user)
     visit user_url(user)
     click_link 'Edit Account'
 
@@ -21,6 +22,7 @@ describe "Editing a user" do
   it "does not update the user if it's invalid" do
     user = User.create!(user_attributes)
 
+    sign_in(user)
     visit edit_user_path(user)
 
     fill_in 'Name', with: ""
